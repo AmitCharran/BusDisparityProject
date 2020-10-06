b5ec2d959688dec6f7f4b103e925f157753bcec6
Amit Charran <amit.charran@gmail.com>
2020-10-06 00:43:06 -0400

    added my knowledge on the API key

---
+++ BusProjectWorkspace/Bus_API_Testing/Bus_API_Testing2.py
@@ -0,0 +1,11 @@
+# Here I will show you how to load the saved JSON file
+import json
+
+# load the file into data as json object
+with open('saved_json_data.txt') as json_file:
+    data = json.load(json_file)
+
+
+print(data)
+# Accessing specific value in the data object
+print(data['currentTime'])
