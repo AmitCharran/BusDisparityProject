d4369aee3f98e1d79ae9c7c78e58755eca69f156
Amit Charran <amit.charran@gmail.com>
2020-10-09 19:54:31 -0400

    got all the different line references in a list

--- BusProjectWorkspace/Bus_API_Testing/Amit/THREE_using_the_reduced_json_file.py
+++ BusProjectWorkspace/Bus_API_Testing/Amit/THREE_using_the_reduced_json_file.py
@@ -5,10 +5,23 @@ with open('TextFileFolder/reduced_vehicle_monitoring_file.txt') as json_file:
 
 
 test_data = data[0]['MonitoredVehicleJourney']
-print(test_data.keys())
+
 
 
 # Need function to get all distinct LineRef and map to bus name
+def find_all_distinct_line_ref(json_data):
+    distinct_line_ref = []
+    for x in range(0, len(json_data)):
+        line_ref = json_data[x]['MonitoredVehicleJourney']['LineRef']
+        if not (distinct_line_ref.__contains__(line_ref)):
+            distinct_line_ref.append(line_ref)
+    return distinct_line_ref
+
+
+all_line_ref = find_all_distinct_line_ref(data)
+print(len(all_line_ref))
+for x in all_line_ref:
+    print(x)
 
 # Need function to get all distinct OperatorRef
 
