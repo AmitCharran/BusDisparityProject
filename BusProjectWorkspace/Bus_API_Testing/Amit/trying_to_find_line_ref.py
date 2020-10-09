8d0e43f669ed69146d510d30b472f10edb5f9341
Amit Charran <amit.charran@gmail.com>
2020-10-08 21:39:51 -0400

    Added Vehicle Monitoring information

---
+++ BusProjectWorkspace/Bus_API_Testing/Amit/trying_to_find_line_ref.py
@@ -0,0 +1,18 @@
+import requests
+import json
+
+
+vehicle_monitoring_url = "http://bustime.mta.info/api/siri/vehicle-monitoring.json"
+
+API_key = 'f3cd89cf-147d-40bf-8557-5431e990e24f'
+
+get_all_info_from_vehicle_monitoring = vehicle_monitoring_url + "?key=" + API_key
+
+response = requests.get(get_all_info_from_vehicle_monitoring)
+
+
+data = response.json()
+with open('saved_json_data.txt', 'w') as outfile:
+    json.dump(data, outfile)
+
+
