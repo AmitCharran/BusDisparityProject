8dc5f12238d1e481ca688676cb0237fe00dad4a5
Amit Charran <amit.charran@gmail.com>
2020-10-09 23:17:31 -0400

    managed to write a function that gets passenger count

--- BusProjectWorkspace/Bus_API_Testing/Amit/THREE_using_the_reduced_json_file.py
+++ BusProjectWorkspace/Bus_API_Testing/Amit/THREE_using_the_reduced_json_file.py
@@ -3,8 +3,26 @@ import json
 with open('TextFileFolder/reduced_vehicle_monitoring_file.txt') as json_file:
     data = json.load(json_file)
 
-
 test_data = data[0]['MonitoredVehicleJourney']
+
+print(test_data.keys())
+
+def get_passenger_count(json_data):
+    for x in json_data:
+        test_data_2 = x['MonitoredVehicleJourney']
+        line_ref = test_data_2['LineRef']
+        if 'MonitoredCall' in test_data_2:
+            test_data_3 = test_data_2['MonitoredCall']
+            if 'Extensions' in test_data_3:
+                test_data_4 = test_data_3['Extensions']
+                if 'Capacities' in test_data_4:
+                    print(line_ref + "\t" + str(test_data_4['Capacities']['EstimatedPassengerCount']))
+
+
+
+
+
+
 
 
 
@@ -19,9 +37,25 @@ def find_all_distinct_line_ref(json_data):
 
 
 all_line_ref = find_all_distinct_line_ref(data)
-print(len(all_line_ref))
-for x in all_line_ref:
-    print(x)
+
+# Writing to outfile
+# with open("TextFileFolder/all_line_ref.txt","w") as outfile:
+#     for x in all_line_ref:
+#         outfile.write(x)
+#         outfile.write("\n")
+
 
 # Need function to get all distinct OperatorRef
+def find_all_distinct_operator_ref(json_data):
+    distinct_operator_ref = []
+    for x in range(0, len(json_data)):
+        operator_ref = json_data[x]['MonitoredVehicleJourney']['OperatorRef']
+        if not (distinct_operator_ref.__contains__(operator_ref)):
+            distinct_operator_ref.append(operator_ref)
+    return distinct_operator_ref
+
+# print(find_all_distinct_operator_ref(data))
+
+
+
 
