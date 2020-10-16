ec6649a5923c4621a351bc16d2216c1c5fa25879
Amit Charran <amit.charran@gmail.com>
2020-10-15 21:06:21 -0400

    making changes

--- BusProjectWorkspace/Bus_API_Testing/Amit/ONE_generating_vehicle_monitoring_information.py
+++ BusProjectWorkspace/Bus_API_Testing/Amit/ONE_generating_vehicle_monitoring_information.py
@@ -6,6 +6,8 @@ vehicle_monitoring_url = "http://bustime.mta.info/api/siri/vehicle-monitoring.js
 
 API_key = 'f3cd89cf-147d-40bf-8557-5431e990e24f'
 
+API_access = "http://bustime.mta.info/api/siri/vehicle-monitoring.json?key=f3cd89cf-147d-40bf-8557-5431e990e24f&VehicleRef=MTABC_6242"
+
 get_all_info_from_vehicle_monitoring = vehicle_monitoring_url + "?key=" + API_key
 
 response = requests.get(get_all_info_from_vehicle_monitoring)
@@ -15,7 +17,7 @@ data = response.json()
 with open('TextFileFolder/saved_json_data.txt', 'w') as outfile:
     json.dump(data, outfile)
 
-with open('TextFileFolder/vehicle_monitoring.json', 'w') as outfile:
+with open('MTA.json', 'w') as outfile:
     json.dump(data, outfile)
 
 
