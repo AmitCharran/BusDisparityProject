0edcdfce2c30943e0384faf72f3e87e1f029446e
Seung Lee <Seung.lee47@gmail.com>
2020-10-27 09:52:27 -0400

    SQLconnection.py:: get cursor

--- BusProjectWorkspace/Bus_API_Testing/Josh/SQLconnection.py
+++ BusProjectWorkspace/Bus_API_Testing/Josh/SQLconnection.py
@@ -1,6 +1,7 @@
 import mariadb
 import sys
 
+# Connect to MariaDB Platform
 try:
     conn = mariadb.connect(
         user="admin",
@@ -14,3 +15,5 @@ except mariadb.Error as e:
     print(f"Error connecting to MariaDB Platform: {e}")
     sys.exit(1)
 
+# Get Cursor
+cur = conn.cursor()
