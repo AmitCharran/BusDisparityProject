b1d3c6d2b388bf0171e96681bfc249871bd95d4f
Seung Lee <Seung.lee47@gmail.com>
2020-11-07 14:15:05 -0500

    getting_json_data.py:: added exception

--- BusProjectWorkspace/getting_json_data.py
+++ BusProjectWorkspace/getting_json_data.py
@@ -13,31 +13,50 @@ get_all_info_from_vehicle_monitoring = vehicle_monitoring_url + "?key=" + API_ke
 starttime = int(round(time.time() * 1000))
 
 counter_calls = 0
+
+def writeToFileException(e):
+    exceptionFile = open('/home/pi/PycharmProjects/BusDisparityProject/JSON_Data/exceptions.txt', 'a')
+    exceptionFile.write(str(starttime) + str(e) + "\n")
+    exceptionFile.close()
+
+
 while True:
-    response = requests.get(get_all_info_from_vehicle_monitoring)
-    data = response.json()
+    try:
+        response = requests.get(get_all_info_from_vehicle_monitoring)
+        data = response.json()
 
-    counter_calls = counter_calls + 1
+        counter_calls = counter_calls + 1
 
-    string_date = data['Siri']['ServiceDelivery']['ResponseTimestamp']
+        string_date = data['Siri']['ServiceDelivery']['ResponseTimestamp']
 
-    temp = string_date[0:string_date.rfind('-')]
+        temp = string_date[0:string_date.rfind('-')]
 
-    date = datetime.strptime(temp, '%Y-%m-%dT%H:%M:%S.%f')
+        date = datetime.strptime(temp, '%Y-%m-%dT%H:%M:%S.%f')
 
-    file_name = str(date.hour) + '_' + str(date.minute) + ".json"
-    directory = '/home/pi/PycharmProjects/BusDisparityProject/JSON_Data/' + str(date.date())
-    if not os.path.isdir(directory):
-        os.mkdir(directory)
+        file_name = str(date.hour) + '_' + str(date.minute) + ".json"
+        directory = '/home/pi/PycharmProjects/BusDisparityProject/JSON_Data/' + str(date.date())
+        if not os.path.isdir(directory):
+            os.mkdir(directory)
 
-    # Output
-    print("Request Call Number: " + str(counter_calls))
-    print(str(starttime) + " " + file_name)
+        # Output
+        print("Request Call Number: " + str(counter_calls))
+        print(str(starttime) + " " + file_name)
 
-    if not os.path.isfile(directory + "/" + file_name):
-        with open(directory + '/' + file_name,
-                  'w') as outfile:
-            json.dump(data, outfile)
+        if not os.path.isfile(directory + "/" + file_name):
+            with open(directory + '/' + file_name,
+                      'w') as outfile:
+                json.dump(data, outfile)
+    except requests.ConnectionError as e:
+        writeToFileException(e)
+    except requests.HTTPError as e:
+        writeToFileException(e)
+    except requests.Timeout as e:
+        writeToFileException(e)
+    except requests.TooManyRedirects as e:
+        writeToFileException(e)
+    except requests.exceptions.RequestException as e:
+        writeToFileException(e)
+
 
     currenttime = int(round(time.time() * 1000))
     # pauseTimeInSecond contains the pause time in seconds
