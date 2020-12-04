64a94065216bf2377262d60f6c5e3a80cc6371dc
arielcebelinski60 <acebelinski@gmail.com>
2020-12-04 16:27:32 -0500

    updating

--- BusProjectWorkspace/CreateDatabaseTesting/Ariel/calculating_distance.py
+++ BusProjectWorkspace/CreateDatabaseTesting/Ariel/calculating_distance.py
@@ -22,30 +22,37 @@ for line in lines:
     if counter >= 4:
         break
 
-print(array)
 
-test_data = array[0]['Longitude']
+def distance_data(dictionary):
+    for x in dictionary:
+            test_data_2 = x['Response Time']
+            latitude_ref = test_data_2['Latitude']
+            longitude_ref = test_data_2['Longitude']
 
+import numpy as np
+import math
+def distance(dist1, dist2):
+ #approximate radius of earth in km
+    R = 6373.0
+    s_lat = dist1['Latitude']
+    s_lng = dist1['Longitude']
+    e_lat = dist2['Latitude']
+    e_lng = dist2['Longitude']
+    s_lat = s_lat * np.pi / 180.0
+    s_lng = np.deg2rad(s_lng)
+    e_lat = np.deg2rad(e_lat)
+    e_lng = np.deg2rad(e_lng)
 
+    d = np.sin((e_lat - s_lat) / 2) ** 2 + np.cos(s_lat) * np.cos(e_lat) * np.sin((e_lng - s_lng) / 2) ** 2
 
-#test_data = array[0]['Response Time']
-#def distance_data(test_data):
- #   for x in test_data:
-  #          test_data_2 = x['Response Time']
-   #         latitude_ref = test_data_2['Latitude']
-    #        longitude_ref = test_data_2['Longitude']
-    #print(distance_data(test_data))
+    return 2 * R * np.arcsin(np.sqrt(d))
 
-#import math
-#def distance(s_lat, s_lng, e_lat, e_lng):
-    # approximate radius of earth in km
- #   R = 6373.0
+print(distance(array[0],array[1]))
 
-  #  s_lat = s_lat * np.pi / 180.0
-   # s_lng = np.deg2rad(s_lng)
-    #e_lat = np.deg2rad(e_lat)
-    #e_lng = np.deg2rad(e_lng)
-
-  #  d = np.sin((e_lat - s_lat) / 2) ** 2 + np.cos(s_lat) * np.cos(e_lat) * np.sin((e_lng - s_lng) / 2) ** 2
-
-   # return 2 * R * np.arcsin(np.sqrt(d))
+def recursive_counter(n):
+    if n == 0:
+        print(n)
+    else:
+         print(n)
+         recursive_counter(n-1)
+recursive_counter(10)
