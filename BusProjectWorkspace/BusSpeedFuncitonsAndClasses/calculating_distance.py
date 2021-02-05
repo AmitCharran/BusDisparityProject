a71912921b32122530680cb78afa59d4575a84b5
Amit C <amitc@Amits-MBP.home>
2021-02-04 23:32:53 -0500

    updating

--- BusProjectWorkspace/BusSpeedFuncitonsAndClasses/calculating_distance.py
+++ BusProjectWorkspace/BusSpeedFuncitonsAndClasses/calculating_distance.py
@@ -63,9 +63,40 @@ class calculating_speed:
             distance_km = self.distance_with_val(latitude[i], longitude[i], latitude[i + 1], longitude[i + 1])
             distance_m = distance_km * 1000
             seconds = self.time_difference(time[i], time[i + 1])
-            answer_array.append((distance_m/seconds))
+            if seconds != 0:
+                answer_array.append((distance_m/seconds))
+            else:
+                answer_array.append(0)
 
         return answer_array
+
+    def convert_to_average_speed_with_check(self, latitude, longitude, time, number_gt):
+        answer_array = []
+
+        for i in range(0, (len(latitude) - 1)):
+            distance_km = self.distance_with_val(latitude[i], longitude[i], latitude[i + 1], longitude[i + 1])
+            distance_m = distance_km * 1000
+            seconds = self.time_difference(time[i], time[i + 1])
+            if seconds != 0:
+                if distance_m/seconds >= number_gt:
+                    print('distance: ' + str(distance_m))
+                    print('seconds: ' + str(seconds))
+                    print('Latitude: {} and {}'.format(str(latitude[i]), str(latitude[i+1])))
+                    print('Longitude: {} and {}'.format(str(longitude[i]), str(longitude[i + 1])))
+                    print('Time: {} and {}'.format(str(time[i]), str(time[i + 1])))
+                answer_array.append((distance_m / seconds))
+            else:
+                answer_array.append(0)
+
+        return answer_array
+
+    def msToMph(self, list):
+        ans = []
+        for x in list:
+            ans.append((x * 2.2369362920544))
+
+        return ans
+
 
     def test(self):
         file = open('Data/output.txt', 'r')
@@ -97,23 +128,139 @@ class calculating_speed:
         answer_dictionary = {}
 
         for key in dictionary.keys():
-            print("Vehicle Ref: " + key)
+            # print("Vehicle Ref: " + key)
             for key_2 in dictionary[key].keys():
-                print("Destination Name: " + key_2)
+                # print("Destination Name: " + key_2)
                 for key_3 in dictionary[key][key_2].keys():
-                    print('Journey Pattern Ref: ' + key_3)
+                    # print('Journey Pattern Ref: ' + key_3)
                     print(dictionary[key][key_2][key_3]['Latitude']) # use latitude, longitude, & time to calculate speed
                     print(dictionary[key][key_2][key_3]['Longitude'])
                     print(dictionary[key][key_2][key_3]['Response Time'])
                     array = self.convert_to_average_speed(dictionary[key][key_2][key_3]['Latitude'],
                                                           dictionary[key][key_2][key_3]['Longitude'],
                                                           dictionary[key][key_2][key_3]['Response Time'])
+                    print("speed:" + str(self.msToMph(array)))
 
 
+    def counting_data(self):
+        file = open('Data/output3.txt', 'r')
+        line = file.readline()
+        file.close()
+        dictionary = ast.literal_eval(line)
 
+        counter = 0
+        for key in dictionary.keys():
+            counter = counter + 1
+            for key_2 in dictionary[key].keys():
+                counter = counter + 1
+                for key_3 in dictionary[key][key_2].keys():
+                    counter = counter + 1
+                    for key_4 in dictionary[key][key_2][key_3].keys():
+                        counter = counter + 1
+        print(counter)
 
+    def test3(self):
+        # this is the one I will use
+        file = open('Data/output3.txt', 'r')
+        line = file.readline()
+        file.close()
+        dictionary = ast.literal_eval(line)
 
+        answer_dictionary = {}
+
+        for key in dictionary.keys():
+            # published line ref
+            if key not in answer_dictionary.keys():
+                answer_dictionary[key] = {}
+            for key_2 in dictionary[key].keys():
+                # Vehicle Ref
+                if key_2 not in answer_dictionary[key].keys():
+                    answer_dictionary[key][key_2] = {}
+                for key_3 in dictionary[key][key_2].keys():
+                    # destination ref
+                    if key_3 not in answer_dictionary[key][key_2].keys():
+                        answer_dictionary[key][key_2][key_3] = {}
+                    for key_4 in dictionary[key][key_2][key_3].keys():
+                        # journey pattern ref
+
+                        array = self.convert_to_average_speed(dictionary[key][key_2][key_3][key_4]['Latitude'],
+                                                              dictionary[key][key_2][key_3][key_4]['Longitude'],
+                                                              dictionary[key][key_2][key_3][key_4]['Response Time'])
+                        answer_dictionary[key][key_2][key_3][key_4] = array
+
+        f = open('Data/converted_speed_data_2.txt', 'a')
+        f.write(str(answer_dictionary))
+        f.write('\n')
+        f.close()
+
+    def show_values_from_of_a_line_ref(self, published_line_ref, input_file):
+        file = open(input_file, 'r')
+        line = file.readline()
+        file.close()
+        dictionary = ast.literal_eval(line)
+
+        p = published_line_ref
+        total_counts = 0
+        for key in dictionary[p].keys():
+            v = key
+            for key2 in dictionary[p][v].keys():
+                d = key2
+                for key3 in dictionary[p][v][d].keys():
+                    j = key3
+                    latitude = dictionary[p][v][d][j]['Latitude']
+                    longitude = dictionary[p][v][d][j]['Longitude']
+                    time = dictionary[p][v][d][j]['Response Time']
+                    array = self.convert_to_average_speed_with_check(latitude, longitude, time, 30)
+
+                    total_counts = len(array) + total_counts
+
+                    if len(array) > 0 and max(array) >= 30:
+                        print(p)
+                        print(v)
+                        print(d)
+                        print(j)
+
+                        for i in range(0, len(longitude) - 1):
+                            print('Longitude: {} and {}'.format(longitude[i], longitude[i + 1]))
+                            print('Latitude: {} and {}'.format(latitude[i], latitude[i + 1]))
+                            print('Time: {} and {}'.format(time[i], time[i + 1]))
+                            print('Speed: ' + str(array[i]))
+                            print('\n\n')
+
+                        print(longitude)
+                        print(latitude)
+                        print(time)
+                        print('Speed Array: ' + str(array))
+        print(total_counts)
+
+    def print_highest_per_line_ref(self, input_file):
+        file = open(input_file, 'r')
+        line = file.readline()
+        file.close()
+        dictionary = ast.literal_eval(line)
+
+        for key in dictionary.keys():
+            p = key
+            current_max = 0
+            for key2 in dictionary[p].keys():
+                v = key2
+                for key3 in dictionary[p][v].keys():
+                    d = key3
+                    for key4 in dictionary[p][v][d].keys():
+                        j = key4
+                        array = dictionary[p][v][d][j]
+                        max_in_array = 0
+                        if len(array) > 0:
+                            max_in_array = max(array)
+                        if max_in_array > current_max:
+                            current_max = max_in_array
+
+            print(p + " : " + str(current_max * 2.2369362920544))
 
 
 test = calculating_speed()
-test.test2()
+test.show_values_from_of_a_line_ref('QM21', 'Data/output3.txt')
+# test.print_highest_per_line_ref('Data/converted_speed_data_2.txt')
+#
+# test.show_values_from_of_a_line_ref('M101', 'Data/output3.txt')
+# 65mph --> 29.0567
